41ac525453dd319fca08a9a6b5bd009ce15b97bc
adf-telkomuniv <adf.asd.telkomuniversity@gmail.com>
2016-05-08 20:49:02 +0700

    Merge origin/master

--- src/se7ensekop/SevelSkop.java
+++ src/se7ensekop/SevelSkop.java
@@ -4,7 +4,6 @@
  */
 package se7ensekop;
 
-import java.util.ArrayList;
 
 /**
  *
@@ -18,18 +17,9 @@ public class SevelSkop {
     public static void main(String[] args) {
         // TODO code application logic here
 
-//        String a = "abcdefghij";
-//        String b = a.substring(0, 6);
-//        System.out.println("a = " + a);
-//        System.out.println("b = " + b);
-//        
-//        String c = a.substring(3);
-//        System.out.println(c);
-        
         Game g = new Game();
         Player Player1 = new PlayerHuman();
-        Player Player2 = new PlayerHuman();
-//        Player Player2 = new Player("p2");
+        Player Player2 = new Player("p2");
         Player Player3 = new Player("p3");
         Player Player4 = new Player("p4");
         g.Play(Player1, Player2, Player3, Player4);
